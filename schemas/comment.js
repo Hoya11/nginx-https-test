214267435633dc94621baab3748b93487552445a
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-06 21:51:16 +0900

    aa

---
+++ schemas/comment.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const commentSchema = mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  photoId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  photoAlbumId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+commentSchema.virtual("commentId").get(function () {
+  return this._id.toHexString();
+});
+
+commentSchema.set("toJSON", {
+  virtuals: true,
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
---
+++ schemas/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const connect = () => {
+  mongoose.connect(process.env.DB_NAME, { ignoreUndefined: true }).catch((err) => {
+    console.error(err);
+  });
+};
+
+module.exports = connect;
